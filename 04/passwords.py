e71f822100db489ca5f49f907faddbbb3d453180
Hilde Aas Nøst <hilde.nost@gmail.com>
2019-12-04 23:34:20 +0100

    Day 4: New tricks and better comments

--- 04/passwords.py
+++ 04/passwords.py
@@ -3,33 +3,24 @@ from collections import Counter
 min_pwd = 197487
 max_pwd = 673251
 
-def nonincreasing(numbers):
-    return all(numbers[i-1] <= numbers[i] for i in range(1, 6))
+pwds = (str(password) for password in range(min_pwd, max_pwd + 1))
 
 total = sum(
     1
-    for password in range(min_pwd, max_pwd+1)
-    if nonincreasing(str(password))
-    and 2 in Counter(str(password)).values() # PART 2
-    #and len(set(str(password))) < 6 # PART 1
+    for pwd in pwds
+    # The digits need to be non-increasing.
+    # This will also ensure that the 2 similar digits are
+    # indeed adjacent, because if they weren't,
+    # we would have decreasing digits.
+    if list(pwd) == sorted(list(pwd))
+    # PART 2:
+    # The password need to have _exactly_ 2 digits
+    # that are similar.
+    and 2 in Counter(pwd).values() # PART 2
+    # PART 1:
+    # There need to be at least 1 digit
+    # occuring at least 2 times.
+    #and len(set(pwd)) < 6 # PART 1
 )
 
-#total = 0
-#for password in range(min_pwd, max_pwd+1):
-#    pwd = str(password)
-#    count_digits = Counter(pwd)
-#
-#    # The password need to have _exactly_ 2 digits that are
-#    # similar. So, if there are no digits that occur
-#    # exactly 2 times, the number is invalid.
-#    if 2 not in count_digits.values():
-#        continue
-#
-#    # The digits need to be non-increasing.
-#    # This will also ensure that the 2 similar digits are
-#    # indeed adjacent, because if they weren't,
-#    # we would have decreasing digits.
-#    if nonincreasing(pwd):
-#        total += 1
-#
 print(total)
