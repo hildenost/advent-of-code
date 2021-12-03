0b6675c35691c9828e3265eddfdd7902e6fe9d6a
Hilde Aas Nøst <hnost@oceaneering.com>
2021-12-03 08:59:00 +0100

    Day 3: Binary Diagnostic

--- 2021/03/binary.py
+++ 2021/03/binary.py
@@ -3,42 +3,35 @@
 import numpy as np
 
 with open("input.txt") as f:
-  diagnostic = np.array(
-    [
-    [int(l) for l in line]
-    for line in f.read().splitlines()
-    ]
-  )
+    diagnostic = np.array([[int(l) for l in line] for line in f.read().splitlines()])
 
-print(diagnostic)
+
+def from_binary(arr):
+    return int("".join(str(int(a)) for a in arr), 2)
+
 
 gamma_mask = diagnostic.sum(axis=0) > diagnostic.shape[0] / 2
 
-gamma = int("".join(str(int(m)) for m in gamma_mask), 2)
-epsilon = int("".join(str(int(m)) for m in ~gamma_mask), 2)
+gamma = from_binary(gamma_mask)
+epsilon = from_binary(~gamma_mask)
 
-print(gamma * epsilon)
+print("Part 1:\t", gamma * epsilon)
 
-#part 2
-gamma_mask = diagnostic.sum(axis=0) >= diagnostic.shape[0] / 2
-
-oxygen = diagnostic
-
-i = 0
-while len(oxygen) > 1:
-  oxygen = oxygen[oxygen[:, i] == gamma_mask[i]] 
-  i += 1
-oxygen_rating = int("".join(str(o) for o in oxygen[0]), 2)
-
-co2 = diagnostic
-i = 0
-while len(co2) > 1:
-  co2 = co2[co2[:, i] == ~gamma_mask[i]] 
-  print(co2, ~gamma_mask)
-  i += 1
-print(co2)
-co2_rating = int("".join(str(o) for o in co2[0]), 2)
-print(co2_rating * oxygen_rating)
+# part 2
+from operator import eq, ne
 
 
-  
+def find_rating(arr, op=eq):
+    i = 0
+    while len(arr) > 1:
+        # filter arr based on column i equaling or not equaling
+        # the sum of column i >= half of the length (to find the max occurence)
+        arr = arr[op(arr[:, i], arr[:, i].sum() >= len(arr) / 2)]
+        i += 1
+    return from_binary(arr[0])
+
+
+oxygen_rating = find_rating(diagnostic, op=eq)
+co2_rating = find_rating(diagnostic, op=ne)
+print("Part 2:\t", co2_rating * oxygen_rating)
+
--- README.md
+++ README.md
@@ -16,5 +16,5 @@ Each year can grant you a maximum of 50 stars. I'll try to get them all!
 |  [2018](2018) | 44⭐️ ![Progress](https://progress-bar.dev/44/?scale=50&suffix=⭐️)|
 |   [2019](2019) | 33⭐️ ![Progress](https://progress-bar.dev/33/?scale=50&suffix=⭐️)|
 | ✔️ [2020](2020) | 50⭐️ ![Progress](https://progress-bar.dev/50/?scale=50&suffix=⭐️)|
-|   [2021](2021) | 4⭐️ ![Progress](https://progress-bar.dev/4/?scale=50&suffix=⭐️)|
-|**total** | 185⭐️ ![Progress](https://progress-bar.dev/185/?scale=350&suffix=⭐️)|
+|   [2021](2021) | 6⭐️ ![Progress](https://progress-bar.dev/6/?scale=50&suffix=⭐️)|
+|**total** | 187⭐️ ![Progress](https://progress-bar.dev/187/?scale=350&suffix=⭐️)|
