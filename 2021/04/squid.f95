32de366258e6034681c6a2345975b382c77d2c99
Hilde Aas Nøst <hilde.nost@gmail.com>
2021-12-05 01:30:47 +0100

    Tidied Fortran

--- 2021/04/squid.f95
+++ 2021/04/squid.f95
@@ -2,28 +2,25 @@ program day04
     implicit none
 
     ! Variables related to file I/O
-    integer :: iostat
     integer :: io
-    integer :: length = 0
-    integer :: values_per_line = 1 
 
     ! Problem related variables
     integer, parameter :: N = 100 ! number of numbers
                                   ! and also number of boards
     integer, parameter :: board_size = 25
+
     integer, dimension(N*board_size) :: boards 
     logical, dimension(N*board_size) :: mask
-    logical, dimension(N) :: win_mask
-    character(100) :: line
 
     integer, dimension(N) :: numbers
-
-    logical :: check_board
-    logical :: bingo
-    logical :: first
+    logical, dimension(N) :: win_mask
 
     integer :: i
     integer :: j
+
+    ! External functions type declaration
+    logical :: check_board
+    integer :: findidx
 
     ! Today, we know the sizes of the arrays
     open(newunit=io, file="input.txt", status="old", action="read")
@@ -32,34 +29,70 @@ program day04
 
     
     do i=1,N
+      ! Adding to mask more drawn numbers
       mask = mask .or. boards == numbers(i)
+
+      ! For Part 2
+      ! If only one board that hasn't won left
       if (count(win_mask) == N - 1) then
-        do j=1, N
-          if (.not. win_mask(j)) then
-            exit
-          end if
-        end do
-        win_mask = [(check_board(mask((j-1)*board_size+1:j*board_size)), j=1, N)]
-        print *, "PART 2"
-        print *, numbers(i) * sum(pack(boards((j-1)*board_size + 1:j*board_size), .not. mask((j-1)*board_size + 1:j*board_size)))
+        j = findidx(win_mask, .False., N)
+        print *, "PART 2", answer(j, numbers(i))
+        ! Then we're completely done
         exit
       end if
 
-      win_mask = [(check_board(mask((j-1)*board_size+1:j*board_size)), j=1, N)]
-      if (any(win_mask) .and. .not. first) then
-        first = .True.
-        do j=1, N
-          if (win_mask(j)) then
-            exit
-          end if
-        end do
+      win_mask = [(check_board(select_mask(i)), i=1, N)]
 
-        print *, "PART 1"
-        print *, numbers(i) * sum(pack(boards((j-1)*board_size + 1:j*board_size), .not. mask((j-1)*board_size + 1:j*board_size)))
+      ! For Part 1
+      ! If only one board has won
+      if (count(win_mask) == 1) then
+        j = findidx(win_mask, .True., N)
+        print *, "PART 1", answer(j, numbers(i))
       end if
     end do
 
+contains
+  ! Internal functions inherits scope
+
+  pure function answer(start, num) result(res)
+    integer, intent(in) :: start
+    integer, intent(in) :: num
+    integer :: res
+    
+    res = num * sum(pack(select_board(j), .not. select_mask(j)))
+
+  end function answer
+
+  pure function select_board(start) result(board)
+    integer, intent(in) :: start
+    integer, dimension(board_size) :: board
+
+    board = boards((start-1)*board_size + 1:start*board_size)
+  end function select_board
+
+  pure function select_mask(start) result(board)
+    integer, intent(in) :: start
+    logical, dimension(board_size) :: board
+
+    board = mask((start-1)*board_size + 1:start*board_size)
+  end function select_mask
+
 end program day04
+
+
+pure function findidx(arr, value, n) result(idx)
+  integer, intent(in) :: n
+  logical, dimension(n), intent(in) :: arr
+  logical, intent(in) :: value
+  integer :: idx
+
+  do idx=1, n
+    if (arr(idx) .eqv. value) then
+      exit
+    end if
+  end do
+
+end function findidx
 
 pure function check_board(board) result(bingo)
   logical, dimension(5, 5), intent(in) :: board
