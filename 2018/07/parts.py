148c6166c20742d91b866d6a6ad43185483a8a16
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-01-18 14:08:03 +0100

    2018: Day 7 refactor

--- 2018/07/parts.py
+++ 2018/07/parts.py
@@ -3,6 +3,15 @@
 with open("input.txt") as f:
     instructions = f.read().splitlines()
 
+#instructions = """Step C must be finished before step A can begin.
+#Step C must be finished before step F can begin.
+#Step A must be finished before step B can begin.
+#Step A must be finished before step D can begin.
+#Step B must be finished before step E can begin.
+#Step D must be finished before step E can begin.
+#Step F must be finished before step E can begin.
+#""".splitlines()
+
 import re
 
 from collections import defaultdict
@@ -31,38 +40,49 @@ def topological_sort(children, parents):
                 queue.append(child)
     return "".join(L)
 
-def construct(children, parents, delta=60):
-    # The nodes without incoming edges
-    queue = sorted(set(children) - set(parents))
+def construct(children, parents, n_workers=5, delta=60):
+    n_nodes = len(set(children).union(set(parents)))
     finished = set()
+    # The first nodes without incoming edges
+    queue = sorted(set(children) - set(parents))
     busy = {}
-    workers = [0]*5
-    for s in range(15):
-        print(workers)
-        popped = []
-        for j, w in enumerate(workers):
-            if w > 0:
-                continue
-            print("WORKER AVAILABLE!!!")
-            # Selecting a task
-            queue.sort()
-            print(queue)
+    sec = 0
+    while queue or busy:
+
+        # If there is capacity and there's a queue
+        # assign tasks
+        queue.sort()
+        while len(busy) < n_workers and queue:
             n = queue.pop(0) 
-            print("Worker should start work on ", n)
-            popped.append(n)
+            busy[n] = delta + ord(n) - 64
 
-#            for child in children[n]:
-#                parents[child].discard(n)
-#                if not parents[child] and child not in finished:
-#                    queue.append(child)
+        busy = {n: t-1 for n, t in busy.items() if t > 0}
+        for n, t in busy.items():
+            if t == 0:
+                # Task complete
+                finished.add(n)
 
-            workers[j] = delta + ord(n) - 64
+                for child in children[n]:
+                    parents[child].discard(n)
+                    if not parents[child] and child not in finished and child not in queue and child not in busy: 
+                        queue.append(child)
+        busy = {n: t for n, t in busy.items() if t > 0}
 
-        workers = [w-1 if w > 0 else 0 for w in workers]
+        sec += 1
+
+        if len(finished) == n_nodes:
+            return sec
 
 
 print("Part 1:\t", topological_sort(children, parents))
-print("Part 2:\t", construct(children, parents))
+children = defaultdict(set) 
+parents = defaultdict(set)
+for line in instructions:
+    first, later = re.findall(r"\b[A-Z]\b", line)
+
+    children[first].add(later)
+    parents[later].add(first)
+print("Part 2:\t", construct(children, parents, delta=60))
 
 
 
