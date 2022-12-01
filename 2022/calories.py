5c00e6b91dd5700f12eb9a6ea03458e70a3c3a4a
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-01 09:44:19 +0100

    Delete calories.py

--- 2022/calories.py
+++
@@ -1,8 +0,0 @@
-""" Advent of Code 2022. Day 1: Calorie Counting """
-
-with open("input.txt") as f:
-    calories = [sum(int(b) for b in a.split()) for a in f.read().split("\n\n")]
-
-print(f"Part 1:\t{max(calories)}")
-
-print(f"Part 2:\t{sum(sorted(calories, reverse=True)[:3])}")
