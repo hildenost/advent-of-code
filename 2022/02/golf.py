e11c11df76136f6107a8f1770997d625a8609d67
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-02 19:00:09 +0100

    2022 Day 2 Code golf

--- 2022/02/golf.py
+++ 2022/02/golf.py
@@ -1,1 +1,1 @@
-print([sum(m.find(x[:3:2])+2 for x in open("a"))/2 for m in ["BXCYAZAXBYCZCXAYBZ","BXCXAXAYBYCYCZAZBZ"]])
+[print(sum(m.find(x[:3:2])+2 for x in open("a"))/2) for m in ["BXCYAZAXBYCZCXAYBZ","BXCXAXAYBYCYCZAZBZ"]]
