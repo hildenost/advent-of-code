cdbdea5c13b398d53f72b07b8f1d3bbc22afaafd
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-05 15:31:40 +0100

    Update golf.py

--- 2022/05/golf.py
+++ 2022/05/golf.py
@@ -1,10 +1,8 @@
-import re
 c,m=open("a").read().split("\n\n")
-c,p=c.splitlines(),range(1,34,4)
 def w(r=1):
-    s = [[l[i] for l in c[:-1] if l[i]!= " "] for i in p]
-    for n,f,t in (map(int, re.findall(r"move (\d+) from (\d) to (\d)", d)[0]) for d in m.splitlines()):
-        l,s[f-1]=s[f-1][:n],s[f-1][n:]
-        s[t-1]=[l,l[::-1]][r]+s[t-1]
-    return "".join(a[0]for a in s)
-print(w(), w(r=0))
+ s=[[l[i] for l in c.split("\n")[:-1]if l[i]!=" "]for i in range(1,34,4)]
+ for n,f,t in (map(int,d.split()[1:6:2])for d in m.split("\n")):
+  l,s[f-1]=s[f-1][:n],s[f-1][n:]
+  s[t-1]=[l,l[::-1]][r]+s[t-1]
+ return "".join(a[0]for a in s)
+print(w(),w(r=0))
