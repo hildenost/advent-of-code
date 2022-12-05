568c92ad5528d5447615f22f83e867e7e3d0b012
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-05 15:46:38 +0100

    Update golf.py

--- 2022/05/golf.py
+++ 2022/05/golf.py
@@ -1,6 +1,6 @@
 c,m=open("a").read().split("\n\n")
 def w(r):
- s=[[l[i]for l in c.split("\n")[:-1]if l[i]!=" "]for i in range(1,34,4)]
- for n,f,t in(map(int,d.split()[1:6:2])for d in m.split("\n")):l,s[f-1]=s[f-1][:n],s[f-1][n:];s[t-1]=[l,l[::-1]][r]+s[t-1]
- return"".join(a[0]for a in s)
+ s=[0]+[[l[i]for l in c.split("\n")[:-1]if l[i]!=" "]for i in range(1,34,4)]
+ for n,f,t in(map(int,d.split()[1:6:2])for d in m.split("\n")):l,s[f]=s[f][:n],s[f][n:];s[t]=[l,l[::-1]][r]+s[t]
+ return"".join(a[0]for a in s[1:])
 print(w(1),w(0))
