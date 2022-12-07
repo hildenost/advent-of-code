182fa34ade7d534623aa995060a609d7325b3605
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-07 21:50:21 +0100

    2022 Day 7 Golf

--- 2022/07/golf.py
+++ 2022/07/golf.py
@@ -1,14 +1,8 @@
-s,c,t={},(),0
+s={}
+c=()
 for l in open("a"):
  x,y,*z=l.split()
- if y=="cd":
-  if z[0]=="..":
-   r=s[c]
-   if r<=1e5:t+=r
-   c=c[:-1]
-  else:
-   c+=(*z,)
-   s[c]=0
+ if y=="cd":c=c[:-1]if l[5]=="."else(*c,*z)
  if x.isdigit():
-  for i in range(len(c)):s[c[:i+1]]+=int(x)
-print(t,min(s[n]for n in s if s[n]>s[("/",)]-4e7))
+  for i in range(len(c)):s[c[:i+1]]=int(x)+s.get(c[:i+1],0)
+print(sum(s[n]for n in s if s[n]<1e5),min(s[n]for n in s if s[n]>s[("/",)]-4e7))
