6e7f0f3a1736657293967bcffc98cf6a64bcfb0b
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-07 20:27:19 +0100

    2022 Day 7 Golf

--- 2022/07/golf.py
+++ 2022/07/golf.py
@@ -1,14 +1,14 @@
-s,c,t={},[],0
+s,c,t={},(),0
 for l in open("a"):
  x,y,*z=l.split()
  if y=="cd":
   if z[0]=="..":
-   r=s[tuple(c)]
+   r=s[c]
    if r<=100000:t+=r
-   c.pop()
+   c=c[:-1]
   else:
-   c.append(*z)
-   s[tuple(c)]=0
+   c+=(*z,)
+   s[c]=0
  if x.isdigit():
-  for i in range(len(c)):s[tuple(c[:i+1])]+=int(x)
+  for i in range(len(c)):s[c[:i+1]]+=int(x)
 print(t,min(s[n]for n in s if s[n]>s[("/",)]-4e7))
