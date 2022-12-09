11cc5c3d93aa14132fad0e863f009bcce016193d
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-09 15:44:35 +0100

    Update golf.py

--- 2022/09/golf.py
+++ 2022/09/golf.py
@@ -4,9 +4,6 @@ for k in 2,10:
  for m in open("a"):
   for _ in range(int(m[2:])):
    d,x,y=m[0],*r[0]
-   r[0]=(x+(d=="R")-(d=="L"),y+(d=="U")-(d=="D"))
-   for i in range(1,k):
-    x,y,v,w=*r[i],*r[i-1]
-    if not(-2<v-x<2 and-2<w-y<2):r[i]=(x+(v>x)-(v<x),y+(w>y)-(w<y))
+   r=[(x+(d=="R")-(d=="L"),y+(d=="U")-(d=="D")),*[(x+(v>x)-(v<x),y+(w>y)-(w<y))if not(-2<v-x<2 and-2<w-y<2)else(x,y)for(x,y),(v,w)in zip(r[1:],r)]]
    h|={r[-1]}
  print(len(h))
