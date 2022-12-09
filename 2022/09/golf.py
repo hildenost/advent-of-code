abf4acfce194c33f1f83677c1d0f2467ac05eb09
Hilde Aas Nøst <hilde.nost@gmail.com>
2022-12-09 14:15:47 +0100

    Update golf.py

--- 2022/09/golf.py
+++ 2022/09/golf.py
@@ -1,13 +1,14 @@
 for k in 2,10:
- v=set() 
+ h=set() 
  r=[(0,0)]*k
  for m in open("a"):
   for _ in range(int(m[2:])):
    d=m[0]
    r[0]=(r[0][0]+(d=="R")-(d=="L"),r[0][1]+(d=="U")-(d=="D"))
    for i in range(1,k):
-    if all(-2<a-b<2 for a,b in zip(r[i-1],r[i])):break
-    dx,dy=[max(-1,min(a-b,1))for a,b in zip(r[i-1],r[i])]
-    r[i]=(r[i][0]+dx,r[i][1]+dy)
-   v|={r[-1]}
- print(len(v))
+    x,y=r[i]
+    v,w=r[i-1]
+    if -2<v-x<2 and -2<w-y<2:break
+    r[i]=(x+(v>x)-(v<x),y+(w>y)-(w<y))
+   h|={r[-1]}
+ print(len(h))
