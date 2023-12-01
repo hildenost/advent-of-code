4d449c17553bea9f609c2d802fd1c8a49ab3db89
Hilde Aas Nøst <hilde.nost@gmail.com>
2023-12-01 14:52:05 +0100

    2023 Dag 01 Mer golf

--- 2023/01/golf.py
+++ 2023/01/golf.py
@@ -1,5 +1,4 @@
 import re
-l=open("a").read().split("\n")
-n=[0,"one","two","three","four","five","six","seven","eight","nine"]
-g=[re.sub("(?=("+"|".join(n[1:])+"))",lambda m:str(n.index(m[1]))+m[1][-1],c)for c in l]
-print([sum(int(re.sub("\D","",c)[0]+re.sub("\D","",c)[-1])for c in p)for p in (l,g)])
+l=open("a").read()
+n=["_","one","two","three","four","five","six","seven","eight","nine"]
+print([sum(int(g[0]+g[-1])for g in[re.sub("\D","",c)for c in p.split("\n")])for p in(l,re.sub("(?=("+"|".join(n)+"))",lambda m:str(n.index(m[1]))+m[1][-1],l))])
