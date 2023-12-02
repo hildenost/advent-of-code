6ba02c180f6a1c62fa6e37486beff430fd0f05a0
Hilde Aas Nøst <hilde.nost@gmail.com>
2023-12-02 09:26:41 +0100

    2023 Dag 2 but cleaner

--- 2023/02/cube.py
+++ 2023/02/cube.py
@@ -1,30 +1,29 @@
 """ Advent of Code 2023. Day 2: Cube Conundrum """
 import re
 
-limits = dict(red=12, green=13, blue=14)
 
 with open("input.txt") as f:
     games = f.read().splitlines()
 
-possible = sum(i + 1 for i in range(len(games)))
-for game in games:
-    n, cubes = game.split(":")
-    n = int(re.findall(r"\d+", n)[0])
-    for subset in cubes.split(";"):
-        cubes = re.findall(r"(\d+) (blue|red|green)", subset)
-        check = [int(i) > limits[color] for i, color in cubes]
-        if any(check):
-            possible -= n
-print("Part 1:\t", possible)
+
+def get_max(cubes, color):
+    return max(int(i) for i, col in cubes if col == color)
 
 
+limits = dict(red=12, green=13, blue=14)
+possible = 0
 power = 0
-for game in games:
-    n, cubes = game.split(":")
-    n = int(re.findall(r"\d+", n)[0])
-    cubes = re.findall(r"(\d+) (blue|red|green)", cubes)
-    blue = max(int(i) for i, col in cubes if col == "blue")
-    green = max(int(i) for i, col in cubes if col == "green")
-    red = max(int(i) for i, col in cubes if col == "red")
+for n, game in enumerate(games, 1):
+    cubes = re.findall(r"(\d+) (blue|red|green)", game)
+
+    blue = get_max(cubes, "blue")
+    green = get_max(cubes, "green")
+    red = get_max(cubes, "red")
+
+    if blue <= limits["blue"] and green <= limits["green"] and red <= limits["red"]:
+        possible += n
+
     power += blue * green * red
+
+print("Part 1:\t", possible)
 print("Part 2:\t", power)
