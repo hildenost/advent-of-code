177865358b5ee7cd08902b48a51843dec88e7e46
Hilde Aas Nøst <hilde.nost@gmail.com>
2023-12-02 20:14:27 +0100

    2023 Dag 2 golf

--- 2023/02/golf.py
+++ 2023/02/golf.py
@@ -2,7 +2,7 @@ import re
 a=b=0
 for n,s in enumerate(open("a").readlines(),1):
  p=o=1
- for c,l in zip("rgb",(12,13,14)):k=max(int(i)for i in re.findall(f"(\\d+) {c}",s));p*=k;o&=k<=l
+ for c,l in zip("rgb",(12,13,14)):k=max(int(i)for i in re.findall("(\d+) "+c,s));p*=k;o&=k<=l
  a+=n*o
  b+=p
 print(a,b)
