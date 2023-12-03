54893461300f309814b08bc8647059f0202dea6b
Hilde Aas Nøst <hilde.nost@gmail.com>
2023-12-03 20:52:29 +0100

    2023 Dag 3 første golf

--- 2023/03/golf.py
+++ 2023/03/golf.py
@@ -4,11 +4,11 @@ w=len(f[0])
 a=(-1,1,-w,w,-w-1,-w+1,w-1,w+1)
 f="".join(f)
 def p(n):return{i for i,s in enumerate(f)if s in n}
-parts={k:int(m[1])for m in re.finditer(r"(\d+)",f)for k in range(*m.span())}
 r=p("=-&$+/@*%#")
 print(sum(int(m[1])for m in re.finditer(r"(\d+)",f)if any(k+b in r for k in range(*m.span())for b in a)))
+q={k:int(m[1])for m in re.finditer(r"(\d+)",f)for k in range(*m.span())}
 def c(i):
- n={parts[i+b] for b in a if i+b in parts}
+ n={q[i+b] for b in a if i+b in q}
  x,y=n if len(n)==2 else(0,0)
  return x*y
 print(sum(c(j)for j in p("*")))
