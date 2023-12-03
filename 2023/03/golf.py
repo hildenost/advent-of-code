f3e73092dc10f27d3f65d148e31ca316c2a66cc3
Hilde Aas Nøst <hilde.nost@gmail.com>
2023-12-03 22:10:18 +0100

    2023 Dag 3 andre golf

--- 2023/03/golf.py
+++ 2023/03/golf.py
@@ -3,12 +3,12 @@ f=open("a").readlines()
 w=len(f[0])
 a=(-1,1,-w,w,-w-1,-w+1,w-1,w+1)
 f="".join(f)
-def p(n):return{i for i,s in enumerate(f)if s in n}
+p=lambda n:{i for i,s in enumerate(f)if s in n}
 r=p("=-&$+/@*%#")
-print(sum(int(m[1])for m in re.finditer(r"(\d+)",f)if any(k+b in r for k in range(*m.span())for b in a)))
-q={k:int(m[1])for m in re.finditer(r"(\d+)",f)for k in range(*m.span())}
+print(sum(int(m[0])for m in re.finditer("\d+",f)if any(k+b in r for k in range(*m.span())for b in a)))
+q={k:int(m[0])for m in re.finditer("\d+",f)for k in range(*m.span())}
 def c(i):
- n={q[i+b] for b in a if i+b in q}
+ n={q[i+b]for b in a if i+b in q}
  x,y=n if len(n)==2 else(0,0)
  return x*y
 print(sum(c(j)for j in p("*")))
