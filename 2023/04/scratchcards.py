4600418566fc1de0c53c0535d0c7ddf9bbdbf0d5
Hilde Aas Nøst <hilde.nost@gmail.com>
2023-12-04 22:46:05 +0100

    2023 dag 4

--- 2023/04/scratchcards.py
+++ 2023/04/scratchcards.py
@@ -5,7 +5,6 @@ with open("input.txt") as f:
     cards = f.readlines()
 
 instances = defaultdict(lambda: 1)
-
 points = 0
 for i, card in enumerate(cards, 1):
     numbers = card.split()
@@ -16,8 +15,8 @@ for i, card in enumerate(cards, 1):
 
     points += 2 ** (n - 1) if n > 0 else 0
 
-    p = instances[i]
     for j in range(i + 1, i + 1 + n):
-        instances[j] += p * 1
+        instances[j] += instances[i]
+
 print("Part 1:\t", points)
 print("Part 2:\t", sum(instances.values()))
