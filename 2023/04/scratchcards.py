1d0fe7c2d9995540ddb576a8933bfe401ab447fb
Hilde Aas Nøst <hilde.nost@gmail.com>
2023-12-04 07:06:58 +0100

    2023 Dag 4

--- 2023/04/scratchcards.py
+++ 2023/04/scratchcards.py
@@ -1,28 +1,23 @@
 """ Advent of Code 2023. Day 4: Scratchcards """
+from collections import defaultdict
 
 with open("input.txt") as f:
     cards = f.readlines()
 
-points = 0
-for card in cards:
-    numbers = card.split()
-    winning = {int(n) for n in numbers[2:12]}
-    yours = {int(n) for n in numbers[13:]}
-
-    n = len(winning & yours)
-    points += 2 ** (n - 1) if n > 0 else 0
-print("Part 1:\t", points)
-
-from collections import defaultdict
-
 instances = defaultdict(lambda: 1)
+
+points = 0
 for i, card in enumerate(cards, 1):
     numbers = card.split()
-    winning = {int(n) for n in numbers[2:12]}
-    yours = {int(n) for n in numbers[13:]}
+    winning = {int(j) for j in numbers[2:12]}
+    yours = {int(j) for j in numbers[13:]}
 
     n = len(winning & yours)
+
+    points += 2 ** (n - 1) if n > 0 else 0
+
     p = instances[i]
     for j in range(i + 1, i + 1 + n):
         instances[j] += p * 1
+print("Part 1:\t", points)
 print("Part 2:\t", sum(instances.values()))
