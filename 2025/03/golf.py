457521a75e0f876856a08f487228349bdabcc832
Hilde Aas Nøst <hilde.nost@gmail.com>
2025-12-03 15:03:02 +0100

    2025 Day 3 golf

--- 2025/03/golf.py
+++ 2025/03/golf.py
@@ -1,4 +1,3 @@
 j=lambda n:sum(int(f(n,b))for b in open("a"))
-def f(n,b):
- m=max(b);l,r=b.split(m,1);return m if n<2 else m+f(n-1,r)if n<=len(r)+1 else f(n-len(r)-1,l)+m+r
+def f(n,b):m=max(b);l,r=b.split(m,1);return m if n<2 else m+f(n-1,r)if n<=len(r)+1 else f(n-len(r)-1,l)+m+r
 print(j(2),j(12))
