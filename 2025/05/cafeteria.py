90bdb107d151708e1d2d62975a64e109537a63fc
Hilde Aas Nøst <hilde.nost@gmail.com>
2025-12-05 19:54:40 +0100

    2025 Day 5

---
+++ 2025/05/cafeteria.py
@@ -0,0 +1,53 @@
+""" Advent of Code 2025. Day 5: Cafeteria """
+
+with open("input.txt") as f:
+    ranges, ids = f.read().split("\n\n")
+
+ranges = [tuple(int(n) for n in r.split("-")) for r in ranges.splitlines()]
+
+n_fresh = 0
+for i in ids.splitlines():
+    for l, u in ranges:
+        if l <= int(i) <= u:
+            n_fresh += 1
+            break
+print("Part 1:\t", n_fresh)
+
+
+ranges = sorted(ranges)
+merged = set()
+
+while ranges:
+    l, u = ranges.pop()
+
+    new_merged = merged
+
+    for a, b in sorted(merged):
+        if a <= l <= u <= b:
+            # New range included in tracked range
+            break
+        elif l <= a <= b <= u:
+            # New range enclosing tracked range
+            new_merged.remove((a, b))
+            new_merged.add((l, u))
+            break
+        elif a <= l <= b:
+            # Lower boundary in tracked range
+            new_merged.remove((a, b))
+            new_merged.add((a, u))
+            break
+        elif a <= u <= b:
+            # Upper boundary in tracked range
+            new_merged.remove((a, b))
+            new_merged.add((l, b))
+            break
+    else:
+        # If for loop not ended with break
+        # range outside of tracked ranges
+        new_merged.add((l, u))
+
+
+    merged = new_merged
+
+
+print("Part 2:\t", sum(u-l+1 for l, u in merged))
