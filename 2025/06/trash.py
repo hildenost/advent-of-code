69fa856c3206937f3e00c98ef850a41b39fd5f3d
Hilde Aas Nøst <hilde.nost@gmail.com>
2025-12-06 14:41:54 +0100

    2025 Day 6

--- 2025/06/trash.py
+++ 2025/06/trash.py
@@ -1,43 +1,32 @@
 """ Advent of Code 2025. Day 6: Trash Compactor"""
+from math import prod
 
 with open("input.txt") as f:
     *numbers, ops = f.read().splitlines()
 
+def to_numbers(numbers):
+    return [[int(n) for n in row.split()] for row in numbers]
 
-numbers = [[int(n) for n in row.split()] for row in numbers]
-ops = ops.split()
+def transpose(numbers):
+    """Transpose the input and split into groups"""
+    return to_numbers("\n".join(
+        "" if not any(ns) else "".join(ns).strip()
+        # must strip to be able to split correctly later
+        for ns in zip(*numbers)
+        ).split("\n\n"))
+
+funcs = [sum if op == "+" else prod for op in ops.split()] 
     
 rowsum = sum(
-    i+j+k+l if op == "+" else i*j*k*l
-    for i, j, k, l, op in zip(*numbers, ops)
+    func(t)
+    for *t, func in zip(*to_numbers(numbers), funcs)
 )
 print("Part 1:\t", rowsum)
 
-with open("input.txt") as f:
-    *numbers, ops = f.read().splitlines()
-cols = [[len(n) for n in row.split()] for row in numbers]
-# Need to find max length per column
-cols = [max(a, b, c, d) for a, b, c, d in zip(*cols)]
-ops = ops.split()
 
-rowsum = 0
-idx = 0
-for i in range(1000):
-    colsize = cols[i]
-
-    ns = []
-    for row in numbers:
-        ns.append(row[idx:idx+colsize])
-
-    temp = 0 if ops[i] == "+" else 1
-    for j in range(colsize):
-        a = int(''.join(r[j] for r in ns))
-        if ops[i] == "+":
-            temp += a 
-        elif ops[i] == "*":
-            temp *= a
-    rowsum += temp
-
-    idx += cols[i] + 1
+rowsum = sum(
+    func(t)
+    for t, func in zip(transpose(numbers), funcs)
+)
 print("Part 2:\t", rowsum)
 
