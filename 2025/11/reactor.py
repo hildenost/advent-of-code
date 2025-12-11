7ede5f73f53d77aec138fc3b7524b8f210ee4c35
Hilde Aas Nøst <hilde.nost@gmail.com>
2025-12-11 12:14:33 +0100

    2025 day 11

--- 2025/11/reactor.py
+++ 2025/11/reactor.py
@@ -1,8 +1,24 @@
-""" Advent of Code 2025. Day 11: Reactor """
+"""Advent of Code 2025. Day 11: Reactor"""
 
 with open("input.txt") as f:
     lines = f.read().splitlines()
 
+example = """\
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+""".splitlines()
+# lines = example
 
 graph = {}
 for line in lines:
@@ -23,38 +39,62 @@ while stack:
 
     stack.extend(graph[node])
 
-print("Part 1: \t", paths)
+print("Part 1:\t", paths)
 
-start = "svr"
-stack = [(start, (start,))]
-visited = set()
-paths = 0
-while stack:
-    print(len(stack))
-    node, path = stack.pop()
-    #print(node, path)
+from collections import defaultdict
 
-    visited.add(path)
-
-    if node == "out":
-        print(path)
-
-
-        if "dac" in path and "fft" in path:
-            paths += 1
-
-        #input()
-        continue
-
+# Create edges
+edges = set()
+for node in graph:
     for child in graph[node]:
-        #print(path, (child, ))
-        #print(path + (child, ))
-        new_path = path + (child, )
-        if new_path in visited:
-            print("BEEN")
-            input()
-            continue
-        stack.append((child, new_path))
+        edges.add(frozenset((node, child)))
 
-print("Part 2: \t", paths)
+# Going backwards
+parents = defaultdict(set)
+for node in graph:
+    for child in graph[node]:
+        parents[child].add(node)
 
+# Do topological sorting
+start = "svr"
+S = {start}
+L = []
+
+while S:
+    n = S.pop()
+    if n == "out":
+        continue
+    L.append(n)
+    for child in graph[n]:
+        edges.remove({child, n})
+        if not any({parent, child} in edges for parent in parents[child] - {n}):
+            S.add(child)
+assert len(L) == len(graph)
+L.append("out")
+
+n_parents = {child: len(p) for child, p in parents.items()}
+
+
+def count(start, end):
+    test_paths = defaultdict(int)
+
+    __, i = start
+    endnode, __ = end
+
+    test_paths[L[i]] = 1
+    for node in L[i + 1 :]:
+        test_paths[node] = sum(test_paths[p] for p in parents[node])
+        if node == endnode:
+            return test_paths[node]
+
+
+# Find whether fft comes before dac in the topological sort
+# or the other way round
+fft_i = L.index("fft")
+dac_i = L.index("dac")
+nodes = {"svr": 0, "dac": dac_i, "fft": fft_i, "out": len(L) - 1}
+sorted_nodes = sorted(nodes.items(), key=lambda n: n[1])
+import math
+
+paths = math.prod(count(a, b) for a, b in zip(sorted_nodes, sorted_nodes[1:]))
+print("Part 2:\t", paths)
