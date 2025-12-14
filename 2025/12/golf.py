27e5cc075fdf89e016fb18abb3e544b78bff3100
Hilde Aas Nøst <hilde.nost@gmail.com>
2025-12-14 12:25:32 +0100

    2025 Day 12 golf

--- 2025/12/golf.py
+++ 2025/12/golf.py
@@ -1,2 +1,2 @@
 i=int
-print(sum(i(x[:2])*i(x[3:5])>=9*sum(map(i,x[7:].split()))for x in open("a")if "x" in x))
+print(sum(i(x[:2])*i(x[3:5])>=9*sum(map(i,x[7:].split()))for x in open("a")if"x"in x))
