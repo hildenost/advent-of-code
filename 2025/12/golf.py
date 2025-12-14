258b413700c2eccc1aa0bd1ea67abca9ab8864ff
Hilde Aas Nøst <hilde.nost@gmail.com>
2025-12-14 12:11:53 +0100

    2025 Day 12 golf

--- 2025/12/golf.py
+++ 2025/12/golf.py
@@ -1,4 +1,2 @@
 import re
-*_,a=open("a").read().split("\n\n")
-a=[[int(n)for n in re.findall(r"\d+",t)]for t in a.splitlines()]
-print(sum(t[0]*t[1]>=9*sum(t[2:])for t in a))
+print(sum(map(lambda x:x[0]*x[1]>=9*sum(x[2:]),[list(map(int,re.findall(r"\d+",t)))for t in open("a").read().split("\n")if"x"in t])))
